4c529bf8172c7b00c358c910f743a991f2ccc089
Deric Prado <dericprado@gmail.com>
2026-06-02 14:28:23 -0300

    Adicionando as experiencias

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileComponent} from '../app/components/perfil/perfil'
+import { ExperienceComponent } from '../app/components/experiencia/experience';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileComponent],
+  imports: [RouterOutlet, ProfileComponent, ExperienceComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- portfolio/src/app/components/perfil/perfil.ts
+++ portfolio/src/app/components/perfil/perfil.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioService } from '../../services/portfolio';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -12,13 +13,16 @@ import { PortfolioService } from '../../services/portfolio';
 export class ProfileComponent implements OnInit {
   profileData: any;
 
-  constructor(private portfolioService: PortfolioService) { }
+  constructor(
+    private portfolioService: PortfolioService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
     this.portfolioService.getProfile().subscribe({
       next: (data: any) => {
-        console.log('Chegou do JSON:', data);
         this.profileData = data;
+        this.cdr.detectChanges();
       },
       error: (err: any) => {
         console.error('Erro ao carregar o perfil:', err);
